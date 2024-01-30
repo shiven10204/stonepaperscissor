0ec113ffc2c8f7c26b7580bc4680520019b53926
Shiven Aggarwal <83549379+shiven10204@users.noreply.github.com>
2024-01-30 23:06:39 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,67 @@
+console.log('chutiya');
+
+
+let comp;
+let compvall;
+let uscore=0;
+let cscore=0;
+turn=0;
+
+function compval(){
+    let num = 3*Math.random();
+    console.log(num);
+    if(num>0 && num<1){
+comp='rock'
+    }
+   else if(num>1 && num<2){
+        comp='paper'
+            }
+            else{
+                comp='scissor'
+            }
+            
+return comp;
+
+}
+function choose(val){
+    turn++;
+compvall=compval();
+
+console.log(' computer chose '+ compvall);
+if(turn==3){
+    document.getElementById('Msg').value='game ends';
+if(uscore>cscore){
+    document.getElementById('declare').innerHTML ='User Wins';
+
+}else if(uscore<cscore){
+    document.getElementById('declare').innerHTML ='Computer Wins';
+
+}else{
+    document.getElementById('declare').innerHTML ='Draw';
+
+}
+
+}else{
+if(val==compvall){
+    console.log('draw')
+    document.getElementById('Msg').innerHTML='Draw';
+
+}else{
+if((val=='rock'&& compvall=='paper') || (val=='paper'&& compvall=='scissor') ||(val=='scissor'&& compvall=='rock')){
+    console.log('computer wins');
+   cscore++;
+    document.getElementById('Msg').innerHTML='computer wins';
+    document.getElementById('cscore').innerHTML=cscore ;
+
+
+}
+else{
+    console.log('user wins');
+    uscore++;
+    document.getElementById('Msg').value='user wins';
+    document.getElementById('uscore').innerHTML =uscore;
+
+}
+}
+}
+}
